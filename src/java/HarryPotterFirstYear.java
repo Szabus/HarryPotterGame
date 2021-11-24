069a64b835ca4a96a70900d6a66ff788386b4fde
Szabus <szendroi.szabina@gmail.com>
2021-11-24 13:02:17 +0100

    clean

--- src/java/HarryPotterFirstYear.java
+++ src/java/HarryPotterFirstYear.java
@@ -1,6 +1,3 @@
-import model.Character;
-import model.Player;
-import storyflow.Challenges;
 import storyflow.StoryFlow;
 import utils.Printer;
 
@@ -8,21 +5,7 @@ public class HarryPotterFirstYear {
 
     public static void main(String[] args) {
 
-     //   StoryFlow.run();
-      //  Printer.printTheEnd("src/resources/hp.txt");
-
-        Player player = new Player();
-        player.setScore(10);
-        player.setKnowledge(1);
-
-        Character ron = new Character();
-        Character harry = new Character();
-        Character hermi = new Character();
-        hermi.setKnowledge(2);
-        player.getHelpTeam().add(ron);
-        player.getHelpTeam().add(harry);
-        player.getHelpTeam().add(hermi);
-
-        Challenges.bimbaChallenge(player);
+        StoryFlow.run();
+        Printer.printTheEnd("src/resources/hp.txt");
     }
 }
